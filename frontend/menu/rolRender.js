07d8a7743fd215092c532e868c56cb242869e9ec
NexusJC <jacobocisnerosbrandomyair@gmail.com>
2025-06-01 22:41:27 -0600

    logo botón

--- frontend/menu/rolRender.js
+++ frontend/menu/rolRender.js
@@ -6,9 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuItems = {
         logo: `
             <li>
-                <div class="header-content__logo-container">
+                <a href="/menu/index.html" class="header-content__logo-container" title="Ir al menú">
                     <img src="/img/logo-ecolima.png" alt="Logo">
-                </div>
+                </a>
             </li>
         `,
         home: `<li title="Inicio"><a href="/menu/index.html"><i class="fa-solid fa-house"></i></a></li>`,
