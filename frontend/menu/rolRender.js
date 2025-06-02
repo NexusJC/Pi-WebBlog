d38b62008c807b58cc592a460979c033aed52abc
NexusJC <jacobocisnerosbrandomyair@gmail.com>
2025-06-02 09:59:29 -0600

    cambios en el navbar

--- frontend/menu/rolRender.js
+++ frontend/menu/rolRender.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 ${menuItems.publish}
                 ${menuItems.admin}
                 ${menuItems.userName}
-                ${isMenuPage ? menuItems.logout : ""}
+                ${menuItems.logout}
                 ${menuItems.search}
             `;
             mostrarBienvenida(`Bienvenido administrador, ${name}`);
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 ${menuItems.about}
                 ${menuItems.contact}
                 ${menuItems.userName}
-                ${isMenuPage ? menuItems.logout : ""}
+                ${menuItems.logout}
                 ${menuItems.search}
             `;
             mostrarBienvenida(`¡Hola ${name}, bienvenido de nuevo!`);
