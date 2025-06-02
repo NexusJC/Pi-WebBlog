5013180ab91222079715954648e36f5efffc5399
NexusJC <jacobocisnerosbrandomyair@gmail.com>
2025-06-02 11:53:04 -0600

    cambios en el navbar

--- server.js
+++ server.js
@@ -734,8 +734,9 @@ const { title, content, referencias, mensaje_autor, tags } = fields;
                         <li title="¿Quiénes somos?"><a href="/about-us/aboutUs.html"><i class="fa-solid fa-people-group"></i></a></li>
                         <li title="¡Contáctanos!"><a href="/contact/contact.html"><i class="fa-solid fa-envelope"></i></a></li>
                         <li title="Usuario" id="userMenuItem">
-                          <li><button class="user-button" id="userDisplay"><i class="fa-solid fa-user"></i> Iniciar sesión</button></li>
-                        </li>
+  <button class="user-button" id="userDisplay"><i class="fa-solid fa-user"></i> Iniciar sesión</button>
+</li>
+
                         <li title="Búsquedas">
                             <div class="main-navbar--ctn-icon-search">
                                 <i class="fa-solid fa-magnifying-glass" id="icon-search"></i>
